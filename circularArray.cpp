9b460d0260dc454db47eea583ca1dd7b59ce0b88
akmaalsyh <taufansyah50@gmail.com>
2024-05-30 15:41:53 +0700

    memasukkan perintah display ketika dipilih menu no 3

--- circularArray.cpp
+++ circularArray.cpp
@@ -139,6 +139,12 @@ int main()
                     q.remove();
                     break;
                 }
+
+                case '3' :
+                {
+                    q.display();
+                    break;
+                }
             }
         }
     }
