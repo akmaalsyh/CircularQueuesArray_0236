09bc02c11cda4a61135d8e3c934b13b496938e30
akmaalsyh <taufansyah50@gmail.com>
2024-05-30 15:41:12 +0700

    memasukkan perintah remove ketika dipilih menu no 2

--- circularArray.cpp
+++ circularArray.cpp
@@ -133,6 +133,12 @@ int main()
                     q.insert();
                     break;
                 }
+
+                case '2' :
+                {
+                    q.remove();
+                    break;
+                }
             }
         }
     }
